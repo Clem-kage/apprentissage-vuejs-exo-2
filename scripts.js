909cc97c8a31cd0d90f15a95061de6f54f00bb46
clemk <clemkfr@gmail.com>
2022-11-11 18:26:39 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember:{
+                fname: null,
+                lname: null,
+                instrument: null
+            }
+        }
+    },
+    methods: {
+        addMember: function(){
+            if (this.newMember.fname && this.newMember.lname && this.newMember.instrument ){
+              this.members.push(this.newMember)
+              this.newMember={}
+        }
+        else {
+            alert('champs vides')
+        }
+    }
+
+},
+}
+Vue.createApp(handlingForms).mount('#app');
